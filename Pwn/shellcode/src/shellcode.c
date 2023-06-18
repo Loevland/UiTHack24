0c5109511eba0f76297b078687edb90e18199446
Andreas <alo102@uit.no>
2023-06-18 22:45:24 +0200

    Added shellcode challenge

---
+++ Pwn/shellcode/src/shellcode.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+void ignore_me(){
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stderr, NULL, _IONBF, 0);
+}
+
+void timeout(int signal){
+    if (signal == SIGALRM){
+        printf("You timed out!\n");
+        _exit(0);
+    }
+}
+
+void ignore_me_timeout(){
+    signal(SIGALRM, timeout);
+    alarm(60);
+}
+
+int main(){
+    ignore_me();
+    ignore_me_timeout();
+
+    char input[60] = {0};
+    printf("Maybe this could be useful: %p\n", input);
+    printf("Now give me some input!\n> ");
+    scanf("%s", input);
+    return 0;
+}
