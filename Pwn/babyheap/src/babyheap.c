fa8a7c5f4c88be67354703de73496839deb6baaf
Loevland <alo102@uit.no>
2023-07-19 22:43:10 +0200

    Rewrote babyheap challenge

--- Pwn/babyheap/src/babyheap.c
+++ Pwn/babyheap/src/babyheap.c
@@ -36,40 +36,44 @@ void print_flag(){
     fclose(f);
 }
 
+int get_int(){
+    int input;
+    printf(">> ");
+    scanf("%d%*c", &input);
+    return input;
+}
+
 void menu(){
-    puts("1. Create Guess");
+    puts("1. Create guess");
     puts("2. Delete guess");
     puts("3. View guess");
     puts("4. Generate password");
     puts("5. Guess password");
     puts("6. Exit");
-    printf(">> ");
 }
 
-char *create_guess(int *guess_set){
-    char *guess = malloc(0x20);
-    printf("Enter your guess:\n>> ");
-    fgets(guess, 0x21, stdin);
-    *guess_set = 1;
-    puts("Guess created!\n");
+char *create_guess(){
+    printf("Enter guess size:\n");
+    int size = get_int()+1;
+    if(size <= 0 || size > 0xff){
+        puts("Invalid size!\n");
+        return NULL;
+    }
+    char *guess = malloc(size);
+    printf("Enter guess:\n>> ");
+    fgets(guess, size, stdin);
+    printf("Guess created!\n");
     return guess;
 }
 
-void delete_guess(char **guess, int *guess_set){
-    if (*guess != NULL)
-        free(*guess);
-    *guess_set = 0;
-    puts("Guess deleted!\n");
+void delete_guess(char *guess){
+    free(guess);
+    guess = NULL;
+    printf("Guess deleted!\n");
 }
 
 void view_guess(char *guess){
-    if (guess != NULL){
-        puts("Your guess is:");
-        puts(guess);
-    }
-    else{
-        puts("No guess created!\n");
-    }
+    printf("Your guess is:\n%s\n", guess);
 }
 
 char *generate_password(){
@@ -83,47 +87,39 @@ char *generate_password(){
     return password;
 }
 
-void guess_password(char *guess, char *password, int guess_set){
-    if(guess == NULL || password == NULL || guess_set == 0){
+void guess_password(char *guess, char *password, int guessed){
+    if(guess == NULL || password == NULL || guessed == 0){
         puts("You need to create a guess and generate a password first!\n");
         return;
     }
-    if(!strncmp(guess, password, 0x20)){
-        puts("Correct password, here is your flag!");
+    if(!memcmp(guess, password, sizeof(password))){
+        puts("Correct password!");
         print_flag();
-        exit(0);
-    }
-    else {
+    } else {
         puts("Wrong password!\n");
     }
 }
 
-
-/*
-    1. Create Guess
-    2. Delete guess
-    3. View guess
-    4. Generate password
-    5. Guess password
-    6. Exit
-*/
 int main(){
     ignore_me();
     ignore_me_timeout();
 
-    int input, guess_set = 0;
-    char *password = NULL, *guess = NULL;
+    int input;
+    char *guess = NULL;
+    int guessed = 0;
+    char *password = NULL;
 
     while(1){
         menu();
-        if (scanf("%d%*c", &input) == 0)
-            exit(0);
+        input = get_int();
         switch (input){
             case 1:
-                guess = create_guess(&guess_set);
+                guess = create_guess();
+                guessed = 1;
                 break;
             case 2:
-                delete_guess(&guess, &guess_set);
+                delete_guess(guess);
+                guessed = 0;
                 break;
             case 3:
                 view_guess(guess);
@@ -132,13 +128,13 @@ int main(){
                 password = generate_password();
                 break;
             case 5:
-                guess_password(guess, password, guess_set);
+                guess_password(guess, password, guessed);
                 break;
             case 6:
                 exit(0);
             default:
                 puts("Invalid option!\n");
-                break;
+                return 0;
         }
     }
     return 0;
