5fe49714c73a101f2daff42e2eadd39747ef24f3
Loevland <alo102@uit.no>
2023-08-13 00:09:49 +0200

    Updated challenge

--- Web/traversal/src/app.js
+++ Web/traversal/src/app.js
@@ -4,28 +4,29 @@ const path = require("path");
 const app = express();
 const port = 9001;
 
+app.use(express.static("static"));
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "static/index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/static", (req, res) => {
+app.get("/images", (req, res) => {
   try {
-    var page = path.join(__dirname, "static/", req.query.page);
+    var image = req.query.image.replace(/\.\.\//g, "");
+    var image_path = path.join(__dirname, "static/images/", image);
   } catch (err) {
+    console.log(err);
     res.status(404).send("Invalid request");
     return;
   }
-  fs.stat(page, (err, _) => {
+
+  fs.stat(image_path, (err, _) => {
     if(err == null){
-      res.sendFile(page);
+      res.sendFile(image_path);
     } else {
-      res.status(404).send("Page does not exist");
+      res.status(404).send(image_path + " does not exist");
     }
   });
-});
-
-app.post("/login", (req, res) => {
-  res.send("Login is currently disabled");
 });
 
 app.listen(port, () => {
