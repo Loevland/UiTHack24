6af5c835d205f0b3dff0f0aee76790ad8730d5c9
Loevland <alo102@uit.no>
2023-09-11 23:27:47 +0200

    Added exfiltration chall

---
+++ Rev_eng/exfiltration/README.md
@@ -0,0 +1,9 @@
+# Exfiltration
+> Rev - 200pts
+
+Congratulations, Agent! You've successfully infiltrated the enigmatic alien base and managed to steal their highly classified code. However, it seems that it is written in some weird foreign language. Your next mission is to find out what the code does!
+
+Files:
+- [exfiltrated.js](src/exfiltrated.js)
+
+[Writeup](writeup/README.md)
---
+++ Rev_eng/exfiltration/writeup/encrypt.js
@@ -0,0 +1,26 @@
+//const flag = "UiTHack24{t3st_fl4g}"
+const enc_flag = "3c3d1c29020859064f0f4740072b390a58531a7d"
+
+const encrypt = input => {
+    let encrypted = ""
+    for (let idx = 0; idx < input.length; idx++) {
+        encrypted += (input.charCodeAt(idx) ^ input.charCodeAt(idx+1 % input.length)).toString(16).padStart(2, "0");
+    }
+    return encrypted
+}
+
+// ask the user for input and check if it matches the encrypted flag
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('What is the password? ', (answer) => {
+    if (encrypt(answer) === enc_flag) {
+        console.log('Correct!');
+    } else {
+        console.log('Wrong!');
+    }
+    rl.close();
+});
