22a561cf136536cacffd34ee18df70ebc7ab8ebc
ilyaTTT <ilya2000@live.no>
2024-01-18 14:18:30 +0100

    Fiksa strukturen på dir, add challange.yml og READMD.md, add start-up i hoved docker-compose fila

---
+++ Web/admin_access/README.md
@@ -0,0 +1,5 @@
+# Admin Access
+> Web - ???
+
+You have to inflitraite the admin area of this website to get the flag. But how do you access it?
+
--- Web/admin_access/src/backend/create.py
+++ Web/admin_access/src/backend/create.py
@@ -3,6 +3,6 @@ import sqlite3
 conn = sqlite3.connect('database.sqlite')
 c = conn.cursor()
 c.execute('''CREATE TABLE users (username TEXT, password TEXT, flag TEXT)''')
-c.execute("INSERT INTO users (username, password, flag) VALUES ('admin', 'asdfmjpagy0123jgla09+213kasgf032', 'Uitflag_2018{SQL_Injection_is_fun!}')")
+c.execute("INSERT INTO users (username, password, flag) VALUES ('admin', 'asdfmjpagy0123jgla09+213kasgf032', 'UiTHack24{SqL_1nj3ct10n_1s_4m4z1ng}')")
 conn.commit()
 conn.close()
