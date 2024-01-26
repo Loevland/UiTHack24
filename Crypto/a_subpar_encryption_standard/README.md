e59a55e2dfdaa872a23e62a240b948cda6476dda
marius@REALF-A242 <min016@uit.no>
2024-01-26 19:05:17 +0100

    merge with main

---
+++ Crypto/a_subpar_encryption_standard/README.md
@@ -0,0 +1,12 @@
+# A subpar encryption standard
+
+> Crypto - 80pts
+
+While looking around in the remains of an enemy spaceship you find some code.
+In their hurry they must have forgotten that there were more steps to do.
+Your priority is to decrypt the code to maybe find some indication of who the ship belonged to.
+
+Files:
+- [flag.txt.enc](src/flag.txt.enc)
+
+[writeup](writeup/README.md)
---
+++ Noob/noob2/src/login.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define USERNAME "noob2"
+#define PASSWORD "UiTHack24{7h3_1337357_0f_3m_4ll}"
+#define USERLEN 5
+#define PASSLEN 32
+
+
+void ignore_me(){
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stderr, NULL, _IONBF, 0);
+}
+
+int main(){
+    ignore_me();
+
+    char user[USERLEN+1];
+    char pass[PASSLEN+1];
+
+    printf("Username: ");
+    scanf("%5s", user);
+
+    printf("Password: ");
+    scanf("%32s", pass);
+
+    if(strncmp(user, USERNAME, USERLEN) == 0 && strncmp(pass, PASSWORD, PASSLEN) == 0){
+        puts("Logged in! Use 'exit' to logout");
+        puts("Here is your shell!\n");
+        system("ls");
+        system("/bin/sh");
+    } else {
+        puts("Invalid Username or Password!");
+    }
+    return 0;
+}
