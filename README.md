6a0f0c1dfad4d7e42f351bd75ab23e91047665f5
Ilya Taksis <35490028+IlyaTTT@users.noreply.github.com>
2024-02-02 12:26:49 +0100

    62 sql injection (#97)
    * har fiksa generelle logikken på backend og frontend, må fikse opp dockerfilene
    
    * Fiksa docker containers
    
    Fiksa docker compose filen slik at man kan build fra den, fiska rikitg config i dockerfilene slik at frontend og backend kan kommunisere.
    
    * Fikse på designedv
    
    Fikse enda på designed. Enda under construction
    
    * Fiksa på frontend designed ferdig
    
    * rename oppgaven
    
    * slett gamle mappa
    
    * Fiksa strukturen på dir, add challange.yml og READMD.md, add start-up i hoved docker-compose fila
    
    * pusha writeup og updatere poengene
    
    * fiksa hoved README.md
    
    * fiksa port. Må enda teste liv på serveren
    
    * fiksa compose
    
    * Fiksa favicon og navn i Taben
    
    ---------
    
    Co-authored-by: Loevland <34869848+Loevland@users.noreply.github.com>

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Add emoji!
 | Noob                | Web                             | Pwn                         | Misc                               | Rev_Eng                               | Crypto                              | IRL |
 | ------------------- | ------------------------------- | --------------------------- | ---------------------------------- | ------------------------------------- | ----------------------------------- | --- |
 | noob1 :baby_bottle: | Space Inspector :eyes:          | Codebreaker :fax:           | Location :round_pushpin:           |                                       | Cosmic Caesar :dizzy:               |     |
-| noob2 :baby:        |                                 | Radiation shield :computer: | Needle in the Haystack 🪡🪡        | Expired License :key:                 | Alienware :closed_lock_with_key:    |     |
+| noob2 :baby:        | Admin access :guardsman:        | Radiation shield :computer: | Needle in the Haystack 🪡🪡        | Expired License :key:                 | Alienware :closed_lock_with_key:    |     |
 | noob3 :cat:         | Space traversal :space_invader: | Xenithian doorway :door:    | Hidden Covenant Info :mag:         | Infiltration :video_game:             | Rapid Encryption :zap:              |     |
 | noob4 :crocodile:   |                                 | Archive :book:              | Federated Learning 1 :closed_book: | Exfiltration :gift:                   | 3D Cipher :globe_with_meridians:    |     |
 | noob5 :dragon:      | Intergalactic bandit :underage: | The great escape :rocket:   | Federated Learning 2 :orange_book: |                                       | A subpar encryption standard :ship: |     |
---
+++ Web/admin_access/src/frontend/app/LoginForm.js
@@ -0,0 +1,105 @@
+"use client";
+
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function LoginForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const userCredentials = { username, password };
+
+
+    try {
+      const response = await fetch("http://uithack.td.org.uit.no:5000/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `username=${encodeURIComponent(
+          username
+        )}&password=${encodeURIComponent(password)}`,
+      });
+
+
+
+      const responseText = await response.text();
+
+      if (responseText === "Login failed!") {
+        throw new Error("Login failed");
+      }
+      else if (responseText === "Naughty naughty!") {
+        toast.warning(responseText, {
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          draggable: true,
+        });
+      }
+      else {
+        toast.success(responseText, {
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          draggable: true,
+        });
+      }
+
+
+
+      if (!response.ok) {
+        throw new Error("Login failed");
+      }
+
+
+
+      // Handle successful login here
+    } catch (error) {
+      toast.error("Login failed", {
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        draggable: true,
+      });
+    }
+  };
+
+  return (
+    <form
+      className="flex flex-col items-center justify-center" style={{ height: '50vh' }}
+      onSubmit={handleSubmit}
+    >
+      <div className="flex flex-col">
+        <label className="mb-2">
+          Username:
+          <input
+            className="border p-2 rounded"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label className="mb-2">
+          Password:
+          <input
+            className="border p-2 rounded"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <input
+          className="bg-blue-500 hover:bg-blue-700 text-white p-2 rounded mt-4 transition duration-300 ease-in-out cursor-pointer"
+          type="submit"
+          value="Submit"
+        />
+      </div>
+      <ToastContainer />
+    </form>
+  );
+}
+
+export default LoginForm;
