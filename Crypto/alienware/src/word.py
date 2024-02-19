49bae6c7ada1c8473c8f6ad6eacb41daf87f956b
Sondre <72896681+SondreUM@users.noreply.github.com>
2024-02-19 12:53:42 +0100

    added solve script and formatting (#123)

--- Crypto/alienware/src/word.py
+++ Crypto/alienware/src/word.py
@@ -1,6 +1,6 @@
 """
     Encryptinator 9000
-    Crypto: easy, medium
+    Crypto: medium
     UiTHack 2024
 """
 
@@ -11,12 +11,14 @@ import random
 
 
 def abcd() -> int:
-    return ((((~(random.randint(0, 255))) << 2) ^ 0b10101010) & 0b01010101)
+    return (((~(random.randint(0, 255))) << 2) ^ 0b10101010) & 0b01010101
 
-def efgh(x:int) -> int:
+
+def efgh(x: int) -> int:
     return (((((~(random.randint(0, 4096)))) & 0b10101010) ^ x) | 0b01010101) % 256
 
-def scrambleinator(x:bytes) -> bytes:
+
+def scrambleinator(x: bytes) -> bytes:
     random.seed("NiceTry")
     for i in range(len(x := bytearray(x))):
         x[i] ^= abcd() ^ efgh(i) ^ KEY.encode("utf-8")[i % len(KEY)]
@@ -25,19 +27,21 @@ def scrambleinator(x:bytes) -> bytes:
 
 if __name__ == "__main__":
     if sys.argv[1] != "encrypt":
-        print("SAFEGUARD: All files in the directory will be encrypted. Run with argument 'encrypt' if you accept.")
+        print(
+            "SAFEGUARD: All files in the directory will be encrypted. Run with argument 'encrypt' if you accept."
+        )
         print("Example: python3 word.py encrypt")
         exit()
 
     for n in os.listdir("."):
         if n == sys.argv[0]:
             continue
-        if n.endswith() == ".enc":
+        if n.endswith(".enc"):
             continue
         with open(n, "wb") as f:
             c = f.read()
             f.write(scrambleinator(c))
             os.rename(n, n + ".enc")
-    with open(__name__, "wb") as f:
-        f.write(f.read().replace(b"8008135", b"NiceTry").replace(KEY, KEY.swapcase()))
+    with open(__name__, "w") as f:
+        f.write(f.read().replace("8008135", "NiceTry").replace(KEY, KEY.swapcase()))
     os.remove(__name__)
